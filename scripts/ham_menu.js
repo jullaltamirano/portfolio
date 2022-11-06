4c3e601ccfa3ae407cba2fa57645ee78dca785f6
jullaltamirano <jullaltamiranop@gmail.com>
2022-11-05 20:25:26 -0500

    dark mode

--- scripts/ham_menu.js
+++ scripts/ham_menu.js
@@ -1,7 +1,15 @@
 let $ham_menu = document.querySelector('.menu_container')
 let $ham_button = document.querySelector('.ham_button')
+let $bar1 = document.querySelector('.bar1')
+let $bar2 = document.querySelector('.bar2')
+let $bar3 = document.querySelector('.bar3')
+
+let bars = []
+bars.push($bar1,$bar2,$bar3)
 
 $ham_button.addEventListener('click', () => {
+    bars.forEach(e => {e.classList.toggle('animado')})
+
     if($ham_menu.style.top === '-213px'){
         return $ham_menu.style.top = '60px'
     }     
@@ -9,6 +17,3 @@ $ham_button.addEventListener('click', () => {
         return $ham_menu.style.top = '-213px'
     }
 })
-
-console.log($ham_menu)
-console.log($ham_button)
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,16 +1,41 @@
 let posicionInicial = window.pageYOffset
 let $nav = document.getElementById('nav')
 let $hamb_menu = document.querySelector('.menu_container')
+let $menu_click1 = document.querySelector('.menu_link1')
+let $menu_click2 = document.querySelector('.menu_link2')
+let $menu_click3 = document.querySelector('.menu_link3')
+let $menu_click4 = document.querySelector('.menu_link4')
+let $bar_1 = document.querySelector('.bar1')
+let $bar_2 = document.querySelector('.bar2')
+let $bar_3 = document.querySelector('.bar3')
+
+let bars1 = []
+bars1.push($bar_1,$bar_2,$bar_3)
+
+let links = []
+links.push($menu_click1, $menu_click2, $menu_click3, $menu_click4)
+
 
 window.addEventListener('scroll', () => {
     let posicionActual = window.pageYOffset
 
+    if($nav.style.top === '-3.75em'){
+        bars1.forEach(e => {e.classList.remove('animado')})
+    }
+
     if(posicionInicial >= posicionActual){
         $nav.style.top = '0em'
+        
     } else {
         $nav.style.top = '-3.75em'
-        $hamb_menu.style.top = '-213px'
+        $hamb_menu.style.top = '-213px'        
     }
 
     posicionInicial = posicionActual
 })
+
+links.forEach(e => e.addEventListener('click',() => {
+    bars1.forEach(e => {e.classList.toggle('animado')})
+    $nav.style.top = '-3.75em'
+    $hamb_menu.style.top = '-213px'
+}))
